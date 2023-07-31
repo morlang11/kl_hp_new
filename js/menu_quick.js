3028abcf76a5f1da3fbf44236d59f51202148f72
morlang11 <130956387+morlang11@users.noreply.github.com>
2023-07-31 17:47:50 +0900

    Add files via upload

--- js/menu_quick.js
+++ js/menu_quick.js
@@ -1,7 +1,9 @@
-$(document).ready(function(){
-    var currentPosition = parseInt($(".quickmenu").css("top"));
-    $(window).scroll(function() {
-      var position = $(window).scrollTop(); 
-      $(".quickmenu").stop().animate({"top":position+currentPosition+"px"},1000);
-    });
-  });
+$(document).ready(function() {
+  if (window.innerWidth > 768) {
+      var currentPosition = parseInt($(".quickmenu").css("top"));
+      $(window).scroll(function() {
+          var position = $(window).scrollTop();
+          $(".quickmenu").stop().animate({"top": position + currentPosition + "px"}, 1000);
+      });
+  }
+});
